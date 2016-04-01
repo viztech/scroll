c79cacbabeed423ffde410dacf095a7b047325e4
siqister <siqi.zhu@gmail.com>
2016-04-01 16:28:21 -0400

    In progress

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 /**
  * Created by siqi on 4/1/16.
  */
+
+console.log('Scrolling...');
+
+//A controller that globally manages scroll interaction
+var controller = new ScrollMagic.Controller();
+
+//Then you create a scene
+//Then you register the scene with the controller
+var scene1 = new ScrollMagic.Scene({
+        //specifies options for the scene
+        triggerElement:"#scene-1",
+        duration:3000,
+        triggerHook:'onEnter'
+    })
+    .on('progress',function(e){
+       console.log(e.progress);
+    })
+    .addTo(controller);
